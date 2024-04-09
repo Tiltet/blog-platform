db5ccd01af84104bf0f0e7dd767925c983aec41b
Tiltet <timofeysavinich@mail.ru>
2024-04-10 00:14:48 +0300

    feat: add sonar-project.properties 2.0

--- src/test/java/com/example/blog/service/UserServiceTest.java
+++ src/test/java/com/example/blog/service/UserServiceTest.java
@@ -3,8 +3,6 @@ package com.example.blog.service;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.*;
-
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
