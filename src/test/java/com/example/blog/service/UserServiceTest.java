448c85e5f3789a02db981c8bfd8b6730986d61d0
Tiltet <timofeysavinich@mail.ru>
2024-04-10 01:31:20 +0300

    feat: add UserServiceTest

--- src/test/java/com/example/blog/service/UserServiceTest.java
+++ src/test/java/com/example/blog/service/UserServiceTest.java
@@ -1,9 +1,40 @@
 package com.example.blog.service;
 
+import com.example.blog.entities.User;
+import com.example.blog.repositories.UserRepository;
+import com.example.blog.services.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    public void testAddUser_Success() {
+        User user = new User();
+        user.setUsername("testUser");
+
+        when(userRepository.findByUsername(user.getUsername())).thenReturn(null);
+        when(userRepository.save(user)).thenReturn(user);
+
+        User result = userService.addUser(user);
+
+        verify(userRepository, times(1)).findByUsername(user.getUsername());
+        verify(userRepository, times(1)).save(user);
+
+        Assertions.assertEquals(user, result);
+    }
 
 }
