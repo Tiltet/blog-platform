635c51e1a8f504d6640c7e0c6d8f8cea70899dfe
Tiltet <timofeysavinich@mail.ru>
2024-04-10 18:51:57 +0300

    feat: add UserServiceTest and BlogServiceTest

--- src/test/java/com/example/blog/service/UserServiceTest.java
+++ src/test/java/com/example/blog/service/UserServiceTest.java
@@ -4,7 +4,6 @@ import com.example.blog.entities.Blog;
 import com.example.blog.entities.User;
 import com.example.blog.repositories.BlogRepository;
 import com.example.blog.repositories.UserRepository;
-import com.example.blog.services.BlogService;
 import com.example.blog.services.UserService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -140,21 +139,20 @@ class UserServiceTest {
 
     @Test
     public void testChangeUserEmail_WhenUserExists_ShouldChangeAndReturnUser() {
-        Long userId = 1L;
         String newEmail = "newemail@example.com";
         User existingUser = new User();
-        existingUser.setId(userId);
+        existingUser.setId(1L);
 
         User updatedUser = new User();
-        updatedUser.setId(userId);
+        updatedUser.setId(1L);
         updatedUser.setEmail(newEmail);
 
-        when(userRepository.findById(userId)).thenReturn(Optional.of(existingUser));
+        when(userRepository.findById(1L)).thenReturn(Optional.of(existingUser));
         when(userRepository.save(existingUser)).thenReturn(updatedUser);
 
-        User result = userService.changeUserEmail(userId, updatedUser);
+        User result = userService.changeUserEmail(1L, updatedUser);
 
-        verify(userRepository, times(1)).findById(userId);
+        verify(userRepository, times(1)).findById(1L);
         verify(userRepository, times(1)).save(existingUser);
         Assertions.assertEquals(updatedUser, result);
         Assertions.assertEquals(newEmail, result.getEmail());
@@ -162,40 +160,34 @@ class UserServiceTest {
 
     @Test
     public void testChangeUserEmail_WhenUserDoesNotExist_ShouldThrowException() {
-        // Arrange
-        Long userId = 1L;
         User userToUpdate = new User();
-        userToUpdate.setId(userId);
+        userToUpdate.setId(1L);
 
-        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+        when(userRepository.findById(1L)).thenReturn(Optional.empty());
 
-        // Act & Assert
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            userService.changeUserEmail(userId, userToUpdate);
+            userService.changeUserEmail(1L, userToUpdate);
         });
 
-        verify(userRepository, times(1)).findById(userId);
+        verify(userRepository, times(1)).findById(1L);
         verify(userRepository, never()).save(any(User.class));
     }
 
     @Test
     public void testAddSubscriber_WhenUserAndBlogExist_ShouldAddSubscriberAndReturnBlog() {
-        Long userId = 1L;
-        Long blogId = 2L;
-
         User existingUser = new User();
-        existingUser.setId(userId);
+        existingUser.setId(1L);
 
         Blog existingBlog = new Blog();
-        existingBlog.setId(blogId);
+        existingBlog.setId(1L);
 
-        when(userRepository.findById(userId)).thenReturn(Optional.of(existingUser));
-        when(blogRepository.findById(blogId)).thenReturn(Optional.of(existingBlog));
+        when(userRepository.findById(1L)).thenReturn(Optional.of(existingUser));
+        when(blogRepository.findById(1L)).thenReturn(Optional.of(existingBlog));
 
-        Blog result = userService.addSubscriber(userId, blogId);
+        Blog result = userService.addSubscriber(1L, 1L);
 
-        verify(userRepository, times(1)).findById(userId);
-        verify(blogRepository, times(1)).findById(blogId);
+        verify(userRepository, times(1)).findById(1L);
+        verify(blogRepository, times(1)).findById(1L);
         verify(userRepository, times(1)).save(existingUser);
         verify(blogRepository, times(1)).save(existingBlog);
 
@@ -206,42 +198,159 @@ class UserServiceTest {
 
     @Test
     public void testAddSubscriber_WhenUserDoesNotExist_ShouldThrowException() {
-        Long userId = 1L;
-        Long blogId = 2L;
-
         Blog existingBlog = new Blog();
-        existingBlog.setId(blogId);
+        existingBlog.setId(1L);
 
-        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+        when(userRepository.findById(1L)).thenReturn(Optional.empty());
 
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            userService.addSubscriber(userId, blogId);
+            userService.addSubscriber(1L, 1L);
         });
 
-        verify(userRepository, times(1)).findById(userId);
-        verify(blogRepository, never()).findById(blogId);
+        verify(userRepository, times(1)).findById(1L);
+        verify(blogRepository, never()).findById(1L);
         verify(userRepository, never()).save(any(User.class));
         verify(blogRepository, never()).save(any(Blog.class));
     }
 
     @Test
     public void testAddSubscriber_WhenBlogDoesNotExist_ShouldThrowException() {
-        Long userId = 1L;
-        Long blogId = 2L;
-
         User existingUser = new User();
-        existingUser.setId(userId);
+        existingUser.setId(1L);
 
-        when(userRepository.findById(userId)).thenReturn(Optional.of(existingUser));
-        when(blogRepository.findById(blogId)).thenReturn(Optional.empty());
+        when(userRepository.findById(1L)).thenReturn(Optional.of(existingUser));
+        when(blogRepository.findById(1L)).thenReturn(Optional.empty());
 
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            userService.addSubscriber(userId, blogId);
+            userService.addSubscriber(1L, 1L);
         });
 
-        verify(userRepository, times(1)).findById(userId);
-        verify(blogRepository, times(1)).findById(blogId);
+        verify(userRepository, times(1)).findById(1L);
+        verify(blogRepository, times(1)).findById(1L);
         verify(userRepository, never()).save(any(User.class));
         verify(blogRepository, never()).save(any(Blog.class));
+    }
+
+    @Test
+    public void testGetAuthorBlogs_WhenUserExists_ShouldReturnBlogs() {
+        User user = new User();
+        List<Blog> expectedBlogs = new ArrayList<>();
+        expectedBlogs.add(new Blog());
+        expectedBlogs.add(new Blog());
+        user.setBlogs(expectedBlogs);
+
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+
+        List<Blog> actualBlogs = userService.getAuthorBlogs(1L);
+
+        Assertions.assertEquals(expectedBlogs, actualBlogs);
+    }
+
+    @Test
+    public void testGetAuthorBlogs_WhenUserDoesNotExist_ShouldThrowException() {
+        when(userRepository.findById(1L)).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            userService.getAuthorBlogs(1L);
+        });
+    }
+
+    @Test
+    public void testGetSubscriptions_WhenUserExists_ShouldReturnSubscriptions() {
+        User user = new User();
+        Set<Blog> expectedSubscriptions = new HashSet<>();
+        expectedSubscriptions.add(new Blog());
+        expectedSubscriptions.add(new Blog());
+        user.setSubscriptions(expectedSubscriptions);
+
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+
+        Set<Blog> actualSubscriptions = userService.getSubscriptions(1L);
+
+        Assertions.assertEquals(expectedSubscriptions, actualSubscriptions);
+    }
+
+    @Test
+    public void testGetSubscriptions_WhenUserDoesNotExist_ShouldThrowException() {
+        when(userRepository.findById(1L)).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            userService.getSubscriptions(1L);
+        });
+    }
+
+    @Test
+    public void testUnsubscribe_WhenUserAndBlogExist_ShouldReturnUpdatedSubscriptions() {
+        User user = new User();
+        user.setId(1L);
+        Set<Blog> userSubscriptions = new HashSet<>();
+        Blog blog = new Blog();
+        blog.setId(1L);
+
+        userSubscriptions.add(blog);
+        user.setSubscriptions(userSubscriptions);
+
+        Set<User> blogSubscribers = new HashSet<>();
+        blogSubscribers.add(user);
+        blog.setSubscribers(blogSubscribers);
+
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        when(blogRepository.findById(1L)).thenReturn(Optional.of(blog));
+
+        Set<Blog> updatedSubscriptions = userService.unsubscribe(1L, 1L);
+
+        Assertions.assertEquals(userSubscriptions, updatedSubscriptions);
+        Assertions.assertFalse(user.getSubscriptions().contains(blog));
+        Assertions.assertFalse(blog.getSubscribers().contains(user));
+    }
+
+    @Test
+    public void testUnsubscribe_WhenUserNotFound_ShouldThrowException() {
+        when(userRepository.findById(1L)).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            userService.unsubscribe(1L, 1L);
+        });
+    }
+
+    @Test
+    public void testUnsubscribe_WhenBlogNotFound_ShouldThrowException() {
+        User user = new User();
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        when(blogRepository.findById(1L)).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            userService.unsubscribe(1L, 1L);
+        });
+    }
+
+    @Test
+    public void testGetAuthor_WhenAuthorsExist_ShouldReturnAuthors() {
+        List<User> expectedAuthors = new ArrayList<>();
+        User author1 = new User();
+        author1.setId(1L);
+        author1.setUsername("testAuthor1");
+        expectedAuthors.add(author1);
+
+        User author2 = new User();
+        author2.setId(2L);
+        author2.setUsername("testAuthor2");
+        expectedAuthors.add(author2);
+
+        when(userRepository.findAllAuthors()).thenReturn(expectedAuthors);
+
+        List<User> actualAuthors = userService.getAuthor();
+
+        Assertions.assertEquals(expectedAuthors, actualAuthors);
+    }
+
+    @Test
+    public void testGetAuthor_WhenNoAuthorsExist_ShouldReturnEmptyList() {
+        List<User> expectedAuthors = new ArrayList<>();
+        when(userRepository.findAllAuthors()).thenReturn(expectedAuthors);
+
+        List<User> actualAuthors = userService.getAuthor();
+
+        Assertions.assertEquals(expectedAuthors, actualAuthors);
     }
 }
