39b308b3e6e684e9700d76eba049c5be1334b8d7
Tiltet <timofeysavinich@mail.ru>
2024-04-20 18:25:24 +0300

    fix: I am clown

--- src/main/frontend/src/App.jsx
+++ src/main/frontend/src/App.jsx
@@ -1,5 +1,6 @@
 // App.jsx
 
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
 import HomePage from "./pages/home/HomePage";
